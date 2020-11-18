fd664b93dc67909a43be21f8a91096b18c670a81
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2020-11-18 17:13:34 +0530

    new text in terminal

---
+++ cooltext.py
@@ -0,0 +1,9 @@
+import os
+from pyfiglet import Figlet
+def cool(text):
+	cooltext=Figlet(font="slant")
+	os.system("cls")
+	return str(cooltext.renderText(text))
+
+if __name__ == "__main__":
+	print(cool("Happy_coding01"))
